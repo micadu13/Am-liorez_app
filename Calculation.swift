9533982bee7477a972df4166ed317235a81f9ad6
michael aidan <mica.aidan@gmail.com>
2019-10-28 22:21:44 +0100

    starting count on me project

---
+++ Calculation.swift
@@ -0,0 +1,12 @@
+//
+//  Calculation.swift
+//  CountOnMe
+//
+//  Created by Michael Aidan on 28/10/2019.
+//  Copyright © 2019 Vincent Saluzzo. All rights reserved.
+//
+
+import Foundation
+class Calculation {
+    
+}
